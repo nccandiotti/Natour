097a6f6dfb72863df0ba3b990a15f12805515bd1
Nicole Candiotti <nccandiotti@gmail.com>
2022-04-28 21:29:00 -0400

    show working

--- app.js
+++ app.js
@@ -28,10 +28,34 @@ app.get("/api/v1/tours", (req, res) => {
     },
   })
 })
+app.get(`/api/v1/tours/:id`, (req, res) => {
+  console.log(req.params)
+  const id = req.params.id * 1
+  const tour = tours.find((tour) => tour.id === id)
+  res.status(200).json({
+    status: "success",
+
+    data: {
+      tour,
+    },
+  })
+})
 
 app.post("/api/v1/tours", (req, res) => {
-  console.log(req.body)
-  res.send("Done")
+  //   console.log(req.body)
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: { tour: newTour },
+      })
+    }
+  )
 })
 
 //Port ------------------------------------
