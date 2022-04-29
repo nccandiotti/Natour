1e790c5a4226094db3120a12e691973b864af731
Nicole Candiotti <nccandiotti@gmail.com>
2022-04-28 20:22:01 -0400

    simple middleware and post working

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const url = require("url")
 const express = require("express")
 
 const app = express()
-
+// Middleware (btwn request and response) --------------------------------
+app.use(express.json())
 // app.get("/", (req, res) => {
 //   res
 //     .status(200)
@@ -14,17 +15,26 @@ const app = express()
 //   res.send("You can post to this endpoint...")
 // })
 
+// Routes ---------------------------------
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
+    results: tours.length,
     data: {
       tours,
     },
   })
 })
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body)
+  res.send("Done")
+})
+
+//Port ------------------------------------
 const port = 3000
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
